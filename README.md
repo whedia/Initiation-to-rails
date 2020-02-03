db6d5f21068605e9a525343ba4f03301376a279e
whedia <hedia.wejdi94@gmail.com>
2020-02-03 15:01:46 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Utilisation : 
+
+- $ git clone git@github.com:notmoebius/sem4j1.git 
+
+ou download zip
+puis
+
+- $ bundle install
+
+- $ shotgun -p 4567
+
+Dans ce projet, j'ai pu créer une base de donnée avec des entrées utilisateur et les afficher dans la page d'acceuil.
+parties non traités : routes dynamique, edition de potins, find et commentaire de potins.
---
+++ lib/controller.rb
@@ -0,0 +1,26 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all} #affiche tout les potins sur la homepage
+  end 
+
+  get '/gossips/new/' do
+    erb :new_gossip #accès au formulaire
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save  
+    redirect '/' #Pour retour à la homepage
+  end
+
+  post '/gossips/new/' do
+    puts "Salut, je suis dans le serveur"
+    puts "Ceci est le contenu du hash params : #{params}"
+    puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+    puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+    puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+    redirect '/' #pour rediriger vers l'acceuil
+  end
+end
